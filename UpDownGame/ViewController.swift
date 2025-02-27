58ea78f72fa324de7f7d0c382c31083be73315e2
InTak Han <hits0304@gmail.com>
2025-02-27 23:34:21 +0900

    first commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by InTak Han on 2/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
