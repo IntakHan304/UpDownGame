0a4ad5221c4700ff9263ace9f5106eea2ebc7ac1
InTak Han <hits0304@gmail.com>
2025-03-05 17:19:03 +0900

    챕터 5 완료

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -29,17 +29,47 @@ class ViewController: UIViewController {
     }
     //동작에 반응하는 액션
     @IBAction func sliderValueChanged(_ sender: UISlider){//슬라이더를 음직일떄 마다 값이 출력됨(실시간 반영)
-        //print(sender.value)
+        print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
+    
+    func showAlert(message: String){
+        let alert = UIAlertController(title: nil , message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.reset()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true,completion: nil)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton){//버튼을 누를때 마다 출력됨
-        /*
-         print(slider.value)
-         */
+        print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            //print("YOU HIT!!")
+            showAlert(message: "YOU HIT!!")
+            reset()
+        }else if tryCount >= 5 {
+            //print("You lose")
+            showAlert(message: "You lose...")
+            reset()
+        }else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = "\(hitValue)"
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = "\(hitValue)"
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender:UIButton){ //리셋 버튼을 누를때 발생하는 이벤트 들
-        //print("touch up reset button")
+        print("touch up reset button")
         reset()
     } //이름을 바꿀땐 그냥 바꾸는 것이 아니라 이름에다가(오른쪽 클릭 후)Refactor -> rename으로 들어가서 바꿀 수 있게 한다
     
