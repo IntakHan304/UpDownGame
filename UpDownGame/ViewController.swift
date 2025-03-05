5df2b43ca3b1c4d7a3604f5472c00a6e69588c28
InTak Han <hits0304@gmail.com>
2025-03-05 16:31:01 +0900

    챕터 4 완료

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     //스토리 보드 요소와 연결할때 쓰는 어노테이션
+    
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +25,7 @@ class ViewController: UIViewController {
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"),for: UIControl.State.normal)
         //움직이는 슬라이더에 이미지 생성
+        reset()
     }
     //동작에 반응하는 액션
     @IBAction func sliderValueChanged(_ sender: UISlider){//슬라이더를 음직일떄 마다 값이 출력됨(실시간 반영)
@@ -31,7 +40,23 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender:UIButton){ //리셋 버튼을 누를때 발생하는 이벤트 들
         //print("touch up reset button")
+        reset()
     } //이름을 바꿀땐 그냥 바꾸는 것이 아니라 이름에다가(오른쪽 클릭 후)Refactor -> rename으로 들어가서 바꿀 수 있게 한다
+    
+    func reset(){
+        print("reset")
+        randomValue = Int.random(in: 0...30)
+        //범위 연산자 a...b까지 a..<b 로 표기되면 a부터 b직전까지(b값은 포함 안됨)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+    }
 }
 
 //주석(Comments) - 프로그래밍을 하면서 코드에 대한 내용을 메모하는 목적으로 사용
